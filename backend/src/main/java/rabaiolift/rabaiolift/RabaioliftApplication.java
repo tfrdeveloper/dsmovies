8198c47feacdbf5337aec6c231f654f4fe59e460
tfrdeveloper <tfrabaioli@outlook.com>
2022-03-07 19:14:43 +0000

    project created

---
+++ backend/src/main/java/rabaiolift/rabaiolift/RabaioliftApplication.java
@@ -0,0 +1,13 @@
+package rabaiolift.rabaiolift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabaioliftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabaioliftApplication.class, args);
+	}
+
+}
